5b86acdff8c6b3564066ea8e80b3947acf2f4cc8
Kieran <kieran@andersons.bz>
2022-11-24 03:45:44 +0000

    Make whitelist more flexible

--- src/Traits/ImmutableTrait.php
+++ src/Traits/ImmutableTrait.php
@@ -4,17 +4,31 @@ namespace KieranFYI\Misc\Traits;
 
 use Illuminate\Database\Eloquent\Model;
 use KieranFYI\Misc\Exceptions\ImmutableModelException;
+use TypeError;
 
 /**
+ * @property array $whitelist
+ *
  * @mixin Model
  */
 trait ImmutableTrait
 {
-
     /**
-     * @var array
+     * Get the policies defined on the provider.
+     *
+     * @return array<class-string, class-string>
      */
-    private array $whitelist = ['deleted_at'];
+    public function policies(): array
+    {
+        if (property_exists($this, 'whitelist')) {
+            if (!is_array($this->whitelist)) {
+                throw new TypeError('Invalid Type on "whitelist", array expected');
+            }
+
+            return $this->whitelist;
+        }
+        return ['deleted_at'];
+    }
 
     /**
      * @param $key
